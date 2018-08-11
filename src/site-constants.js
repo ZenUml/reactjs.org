a20cb482d54bb1245bc179a78b8f210e9875e471
PengXiao <eagle.xiao@gmail.com>
2018-08-11 15:07:50 +1000

    Updated version and github link.

--- src/site-constants.js
+++ src/site-constants.js
@@ -7,8 +7,8 @@
 
 // NOTE: We can't just use `location.toString()` because when we are rendering
 // the SSR part in node.js we won't have a proper location.
-const urlRoot = 'https://reactjs.org';
-const version = '16.4.1';
+const urlRoot = 'https://www.zenuml.com';
+const version = '2.0.0-beta';
 const babelURL = '//unpkg.com/babel-standalone@6.26.0/babel.min.js';
 
 export {urlRoot, version, babelURL};
